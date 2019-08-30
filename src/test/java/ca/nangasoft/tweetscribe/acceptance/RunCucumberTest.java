2253b4b6bf4952aa22c437f3b2f10633f3e73a4e
Kamil Grabowski <grabowski.kamil@gmail.com>
2019-08-30 15:11:51 -0600

    Bootstrap project with test dependencies
    Set up testing infrastructure with:
      * JUnit 5
      * AssertJ
      * Cucumber for Java

---
+++ src/test/java/ca/nangasoft/tweetscribe/acceptance/RunCucumberTest.java
@@ -0,0 +1,14 @@
+package ca.nangasoft.tweetscribe.acceptance;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        snippets = SnippetType.CAMELCASE,
+        features = "src/test/resources"
+)
+public class RunCucumberTest {
+}
